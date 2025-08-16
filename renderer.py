be72e76f5c15f6e39c219c7c9c0bc423b6f92b0b
Sarina NaserMoghadasi <sarina.nasermoghadasi@email.kntu.ac.ir>
2025-08-16 07:24:45 +0330

    24mordad04

--- renderer.py
+++
@@ -1,7 +0,0 @@
-from jinja2 import Environment, FileSystemLoader
-env = Environment(loader = FileSystemLoader('templates'))
-template = env.get_template('end_date_state.jinja')
-output = template.render()
-
-with open("renders/end_date_state_rendered.txt", 'w') as f:
-    print(output, file = f)
